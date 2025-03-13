37d8d202f01780087083cc607f8abed3622ec105
JiaoZenghao <jiaozenghao@gmail.com>
2025-03-13 08:30:47 +0800

    extract info for web page

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
+from newspaper import Article
+
+
 HACKER_NEWS_TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
 HACKER_NEWS_ITEM_URL = "https://hacker-news.firebaseio.com/v0/item/{}.json"
+
 
 def get_top_hacker_news(n=5):
     response = requests.get(HACKER_NEWS_TOP_STORIES_URL)
@@ -9,39 +13,31 @@ def get_top_hacker_news(n=5):
     stories = []
     for story_id in top_story_ids:
         story = requests.get(HACKER_NEWS_ITEM_URL.format(story_id)).json()
-        print(story)
+        source_url = story.get("url")
+        content = extract_story_content(source_url)
         if story:
-            stories.append({"title": story.get("title"), "url": story.get("url")})
+            stories.append(
+                {"title": story.get("title"), "url": source_url, "content": content}
+            )
 
     return stories
 
 
-# import openai
+def extract_story_content(url):
+    article = Article(url)
+    article.download()
+    article.parse()
 
+    return {"title": article.title, "content": article.text[:]}
 
-# def generate_post(topic):
-#     """基于热点话题生成社交媒体文章"""
-#     prompt = f"请基于以下科技新闻标题撰写一篇适合社交媒体的简短文章：{topic}"
-
-#     chain = model | parser
-
-#     messages=[{"role": "system", "content": "你是一个社交媒体内容创作者。"},
-#              {"role": "user", "content": prompt}]
-
-#     return chain.invoke(messages)
-
-# def generate_posts_for_news(news_list):
-#     """为多个新闻生成文章"""
-#     posts = []
-#     for news in news_list:
-#         post = generate_post(news["title"])
-#         posts.append({"title": news["title"], "url": news["url"], "post": post})
-#     return posts
 
 def main():
     stories = get_top_hacker_news()
     print(stories)
 
+
+#    message = generate_post( get_top_hacker_news())
+#    print(message)
 
 if __name__ == "__main__":
     main()
